7c073b6d6cac8b7b0bbf4e0100c1eac4a52fcf45
刘文明 <1824259472@qq.com>
2019-08-13 17:06:36 +0800

    add index and bootstrap

--- src/main/java/com/pcliu/community/controller/HelloController.java
+++ src/main/java/com/pcliu/community/controller/HelloController.java
@@ -1,9 +1,12 @@
 package com.pcliu.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController
-{
-
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "/index";
     }
+}
